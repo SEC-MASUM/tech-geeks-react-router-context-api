f36668bcbe6c975096e378ea4de32fa78934ca96
SEC-MASUM <masum.jess42@gmail.com>
2022-04-08 09:20:25 +0600

    created some components set some routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Videos from "./components/Videos/Videos";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
